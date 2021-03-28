a43dea146a4270fb3984375c03d9cd75c3cfe6b8
kaan168 <kaanozd.x@gmail.com>
2021-03-28 18:07:44 +0300

    first

---
+++ Register/Controllers/VehicleController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Register.M
+namespace Register.Controllers
+{
+    public class VehicleController : Controller
+
+    {
+       
+
+        public ActionResult index()
+        {
+
+            return View();
+        }
+
+        
+    }
+}
---
+++ Register/Models/Vehicle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VehicleRegister.Models
+{
+    public class Vehicle
+    {
+
+        [Key]
+        public int VehichleId { get; set; }
+
+        public string plateOfVehicle;
+
+        public string NicknameOfVehicle;
+
+        public string brandOfVehicle;
+
+        public string modelOfVehicle;
+
+        public string typeOfVehicle;
+
+        public string colorOfVehicle;
+
+        public bool active;
+
+
+
+    }
+}
